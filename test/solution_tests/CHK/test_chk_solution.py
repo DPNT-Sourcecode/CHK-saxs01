dd674f32e0e72b0d484e783771081982e86a04b1
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-10-07 23:37:43 +0000

    Tue Oct 07 23:37:43 UTC 2025

--- test/solution_tests/CHK/test_chk_solution.py
+++ test/solution_tests/CHK/test_chk_solution.py
@@ -9,8 +9,12 @@ class TestSum():
         assert CheckoutSolution().checkout("D") == 15
 
     def test_checkout_multiple_items(self):
-        assert CheckoutSolution().checkout("AB") == 50
-        assert CheckoutSolution().checkout("AB") == 50
+        assert CheckoutSolution().checkout("AB") == 80
+        assert CheckoutSolution().checkout("BA") == 80
+    
+    def test_checkout_offers(self):
+        assert CheckoutSolution().checkout("AAA") == 80
+        assert CheckoutSolution().checkout("AAAA") == 80
 
 
     def test_non_string_input(self):
@@ -18,6 +22,10 @@ class TestSum():
     
     def test_non_existing_item(self):
         assert CheckoutSolution().checkout("F") == -1
+    
+    def test_empty_cart(self):
+        assert CheckoutSolution().checkout("") == 0
+
 
 
 
