653ebc05e1156c3d2abd93ddd6ad3afb884d99f2
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-10-07 22:57:58 +0000

    Tue Oct 07 22:57:58 UTC 2025

--- test/solution_tests/HLO_R1/test_hlo_r1_solution.py
+++ test/solution_tests/HLO_R1/test_hlo_r1_solution.py
@@ -1,6 +1,7 @@
-from solutions.SUM.sum_solution import SumSolution
+from solutions.HLO.hello_solution import HelloSolution
 
 
-class TestSum():
+class TestHloR1():
     def test_sum(self):
-        assert SumSolution().compute(1, 2) == 3
+        assert HelloSolution().compute("world") == "hello world"
+
