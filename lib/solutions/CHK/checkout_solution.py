2ed13a60a714855fe86528f72771ad219b2d6594
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-10-07 23:28:43 +0000

    Tue Oct 07 23:28:43 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -16,7 +16,14 @@ class CheckoutSolution:
         self.total = 0
 
     # skus = unicode string
-    def checkout(self, skus:str) -> int:
+    def checkout(self, skus) -> int:
+        if not isinstance(skus, str):
+            return -1
         
+        if any(item not in self.prices for item in skus):
+            return -1
+        
+
+
 
 
