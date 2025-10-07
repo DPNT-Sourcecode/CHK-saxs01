2cb7278b87166863956e4a3ba7c71cfdca7a1f63
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-10-07 23:10:26 +0000

    Tue Oct 07 23:10:26 UTC 2025

--- test/solution_tests/HLO/test_hlo_r1_solution.py
+++ test/solution_tests/HLO/test_hlo_r1_solution.py
@@ -3,7 +3,8 @@ from solutions.HLO.hello_solution import HelloSolution
 
 class TestHloR1():
     def test_sum(self):
-        assert HelloSolution().hello("World!test") == "Hello, World!test"
+        assert HelloSolution().hello("World!") == "Hello, World!"
+
 
 
 
