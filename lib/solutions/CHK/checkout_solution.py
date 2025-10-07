55677635fe171e890fbfdfe26185c5c9da04355a
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-10-07 23:22:43 +0000

    Tue Oct 07 23:22:43 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,16 @@
 
 class CheckoutSolution:
+    def __init__(self):
+        self.prices = {
+            "A": 50,
+            "B": 30,
+            "C": 20,
+            "D": 15,
+        }
+
+        self.offers
 
     # skus = unicode string
     def checkout(self, skus):
         raise NotImplementedError()
+
--- test/solution_tests/CHK/test_chk_solution.py
+++ test/solution_tests/CHK/test_chk_solution.py
@@ -2,6 +2,9 @@ from solutions.CHK.checkout_solution import CheckoutSolution
 
 
 class TestSum():
-    def test_single_items(self):
-        skus = "ABCD"
-        assert CheckoutSolution().checkout(skus) == 115
+    def test_single_item(self):
+        assert CheckoutSolution().checkout("A") == 50
+        assert CheckoutSolution().checkout("B") == 30
+        assert CheckoutSolution().checkout("C") == 20
+        assert CheckoutSolution().checkout("D") == 15
+
